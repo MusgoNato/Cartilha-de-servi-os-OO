6b0e47e90a489caa10234676eac4893e83bb53dc
Hugo Josué lema das neves <131496781+MusgoNato@users.noreply.github.com>
2024-12-20 08:59:32 -0400

    Corrigido impressoes de servicos

--- src/BD/Servico.php
+++ src/BD/Servico.php
@@ -37,31 +37,31 @@ class Servico extends Banco
             // Quando existir a variavel servico na url, entao mostro as informacoes do servico
             if(isset($_REQUEST['servico']))
             {
-                foreach($servicos as $servico)
-                {
-                    echo "<ul class='list-group'>";
-                        echo "<li class='list-group-item'>". htmlspecialchars($servico['descricao'])."</li>";
-                        echo "<li class='list-group-item>Local de acesso: ". htmlspecialchars($servico['local_de_acesso'])."</li>";
-                        echo "<li class='list-group-item>Canais de acesso: ". htmlspecialchars($servico['canais_de_acesso'])."</li>";
-                        echo "<li class='list-group-item>Forma de solicitação: ". htmlspecialchars($servico['forma_de_solicitacao'])."</li>";
-                        echo "<li class='list-group-item>Publico alvo: ". htmlspecialchars($servico['publico_alvo'])."</li>";
-                        echo "<li class='list-group-item>Categoria do serviço: ". htmlspecialchars($servico['categoria_do_servico'])."</li>";
-                        echo "<li class='list-group-item>Setor inicial: ". htmlspecialchars($servico['setor_inicial'])."</li>";
-                        echo "<li class='list-group-item>Documentos obrigatórios: ". htmlspecialchars($servico['documentos_obrigatorios'])."</li>";
-                        echo "<li class='list-group-item>Legislação: ". htmlspecialchars($servico['legislacao'])."</li>";
-                        echo "<li class='list-group-item>Observações: ". htmlspecialchars($servico['observacoes'])."</li>";
-                        echo "<li class='list-group-item>Tipo: ". htmlspecialchars($servico['tipo'])."</li>";
-                        echo "<li class='list-group-item>Tempo estimado em dias: ". htmlspecialchars($servico['tempo_estimado_dias'])."</li>";
-                        echo "<li class='list-group-item>Custo de serviço: ". htmlspecialchars($servico['custo_de_servico'])."</li>";
-                    echo "</ul>";
-                }
+                // Jogada de indice pra pegar o servico correto
+                $i = ((int)$_REQUEST['servico']) - ((int)$_REQUEST['secretaria']);
+                
+                echo "<ul class='list-group'>";
+                echo "<li class='list-group-item'>". htmlspecialchars($servicos[$i]['descricao'])."</li>";
+                echo "<li class='list-group-item'>Local de acesso: ". htmlspecialchars($servicos[$i]['local_de_acesso'])."</li>";
+                echo "<li class='list-group-item'>Canais de acesso: ". htmlspecialchars($servicos[$i]['canais_de_acesso'])."</li>";
+                echo "<li class='list-group-item'>Forma de solicitação: ". htmlspecialchars($servicos[$i]['forma_de_solicitacao'])."</li>";
+                echo "<li class='list-group-item'>Publico alvo: ". htmlspecialchars($servicos[$i]['publico_alvo'])."</li>";
+                echo "<li class='list-group-item'>Categoria do serviço: ". htmlspecialchars($servicos[$i]['categoria_do_servico'])."</li>";
+                echo "<li class='list-group-item'>Setor inicial: ". htmlspecialchars($servicos[$i]['setor_inicial'])."</li>";
+                echo "<li class='list-group-item'>Documentos obrigatórios: ". htmlspecialchars($servicos[$i]['documentos_obrigatorios'])."</li>";
+                echo "<li class='list-group-item'>Legislação: ". htmlspecialchars($servicos[$i]['legislacao'])."</li>";
+                echo "<li class='list-group-item'>Observações: ". htmlspecialchars($servicos[$i]['observacoes'])."</li>";
+                echo "<li class='list-group-item'>Tipo: ". htmlspecialchars($servicos[$i]['tipo'])."</li>";
+                echo "<li class='list-group-item'>Tempo estimado em dias: ". htmlspecialchars($servicos[$i]['tempo_estimado_dias'])."</li>";
+                echo "<li class='list-group-item'>Custo de serviço: ". htmlspecialchars($servicos[$i]['custo_de_servico'])."</li>";
+                echo "</ul>";
             }
             else
             {
                 foreach($servicos as $servico)
                 {
                     echo "<div class='card' style='width: 18rem;''>";
-                        echo "<a href='?secretaria={$this->id_secretaria}&servico={$servico['ID_servico']}'>" . htmlspecialchars($servico['titulo'])."</a>";
+                    echo "<a href='?secretaria={$this->id_secretaria}&servico={$servico['ID_servico']}'>" . htmlspecialchars($servico['titulo'])."</a>";
                     echo "</div>";
                 }
             }
